f18f49ff318717215155c496ea1afdcda4e2671f
Moises Moreno <it.moisesmoreno@gmail.com>
2024-09-21 10:28:05 -0600

    Init commit

---
+++ src/main/java/com/example/configuration/ConfigurationApplication.java
@@ -0,0 +1,26 @@
+package com.example.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.ConfigurableApplicationContext;
+
+
+
+@SpringBootApplication
+@RestController
+public class ConfigurationApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(ConfigurationApplication.class, args);
+		Customers c = context.getBean(Customers.class);
+		c.display();
+	}
+	
+	@GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
+}
